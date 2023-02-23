448070e804d70528e51c6542f538e1a90faddfd4
A1exsandro <alexsandro.acp@gmail.com>
2023-02-23 16:29:50 -0400

    first test face api

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 
 import React, { useRef, useState } from 'react'; 
 import type {PropsWithChildren} from 'react';
+import FaceSDK, { FaceCaptureResponse } from '@regulaforensics/react-native-face-api';
 import {
   StyleSheet,
   Text,
@@ -30,11 +31,21 @@ function App(): JSX.Element {
   const [box, setBox] = useState(null)
   const cameraRef = useRef(null)
 
+const [img, setImg] = useState<FaceCaptureResponse | undefined>()
+  console.log('========imgSet', img)
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  // FaceSDK.presentFaceCaptureActivity(faceCaptureResponse => {
+  //   const response = FaceCaptureResponse.fromJson(JSON.parse(faceCaptureResponse));
+  //   // ... check response.image.bitmap for capture result.
+
+  //   setImg(response)
+  // }, e => { });
 
   return (
    <View style={styles.container}>
