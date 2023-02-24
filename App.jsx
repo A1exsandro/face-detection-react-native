4784e4a09c8942222e5b4c322f749d458a95f2ba
A1exsandro <alexsandro.acp@gmail.com>
2023-02-24 01:22:35 -0400

    here works fine with function component

--- App.jsx
+++ App.jsx
@@ -1,153 +1,160 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Button, Text, Image, TouchableHighlight, Alert } from 'react-native'
-import { launchImageLibrary } from 'react-native-image-picker';
-import FaceSDK, { Enum, FaceCaptureResponse, LivenessResponse, MatchFacesResponse, MatchFacesRequest, MatchFacesImage, MatchFacesSimilarityThresholdSplit } from '@regulaforensics/react-native-face-api'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-var image1 = new MatchFacesImage()
-var image2 = new MatchFacesImage()
+import React, { useState } from 'react'; 
+import { launchImageLibrary } from 'react-native-image-picker'; 
+import FaceSDK, { 
+  Enum, 
+  FaceCaptureResponse, 
+  MatchFacesResponse, 
+  MatchFacesRequest, 
+  MatchFacesImage, 
+  MatchFacesSimilarityThresholdSplit 
+} from '@regulaforensics/react-native-face-api';
+import {
+  StyleSheet,
+  Text, 
+  View, 
+  Image,
+  Button,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      img1: require('./images/portrait.png'),
-      img2: require('./images/portrait.png'),
-      similarity: "nil",
-      liveness: "nil"
-    }
-  }
+  
 
-  pickImage(first) {
+function App() {  
+  const [imge1, setImge1] = useState(new MatchFacesImage())
+  const [imge2, setImge2] = useState(new MatchFacesImage())
+
+  const [img1, setImg1] = useState({uri: require('./images/portrait.png')})
+  const [img2, setImg2] = useState({uri: require('./images/portrait.png')})
+  const [similarity, setSimilarity] = useState({ similarity: "nil" }) 
+
+  //Capture the images
+  const pickImage = (first) => {
     Alert.alert("Select option", "", [
       {
         text: "Use gallery",
         onPress: () => launchImageLibrary({ includeBase64: true }, response => {
           if (response.assets == undefined) return
-          this.setImage(first, response.assets[0].base64, Enum.ImageType.PRINTED)
+          setImage(first, response.assets[0].base64, Enum.ImageType.PRINTED)
         })
       },
       {
         text: "Use camera",
         onPress: () => FaceSDK.presentFaceCaptureActivity(result => {
-          this.setImage(first, FaceCaptureResponse.fromJson(JSON.parse(result)).image.bitmap, Enum.ImageType.LIVE)
+          setImage(first, FaceCaptureResponse.fromJson(JSON.parse(result)).image.bitmap, Enum.ImageType.LIVE)
         }, e => { })
       }], { cancelable: true })
   }
 
-  setImage(first, base64, type) {
+  // Set path image
+  const setImage = (first, base64, type) => {
     if (base64 == null) return
-    this.setState({ similarity: "nil" })
-    if (first) {
-      image1.bitmap = base64
-      image1.imageType = type
-      this.setState({ img1: { uri: "data:image/png;base64," + base64 } })
-      this.setState({ liveness: "nil" })
+    if (first) { 
+      imge1.bitmap = base64
+      imge1.imageType = type
+      setImg1({uri: { uri: "data:image/png;base64," + base64 }})
     } else {
-      image2.bitmap = base64
-      image2.imageType = type
-      this.setState({ img2: { uri: "data:image/png;base64," + base64 } })
+      imge2.bitmap = base64
+      imge2.imageType = type
+      setImg2({uri: { uri: "data:image/png;base64," + base64 }})
     }
   }
 
-  clearResults() {
-    this.setState({
-      img1: require('./images/portrait.png'),
-      img2: require('./images/portrait.png'),
-      similarity: "nil",
-      liveness: "nil"
-    })
-    image1 = new MatchFacesImage()
-    image2 = new MatchFacesImage()
-  }
-
-  matchFaces() {
-    if (image1 == null || image1.bitmap == null || image1.bitmap == "" || image2 == null || image2.bitmap == null || image2.bitmap == "")
+  // Match
+  const matchFaces = () => {
+    if (imge1 == null || imge1.bitmap == null || imge1.bitmap == "" || imge2 == null || imge2.bitmap == null || imge2.bitmap == "")
       return
-    this.setState({ similarity: "Processing..." })
-    request = new MatchFacesRequest()
-    request.images = [image1, image2]
-    FaceSDK.matchFaces(JSON.stringify(request), response => {
-      response = MatchFacesResponse.fromJson(JSON.parse(response))
+
+    setSimilarity({ similarity: "Processing..." })
+
+    const request = new MatchFacesRequest()
+    request.images = [imge1, imge2] 
+
+    FaceSDK.matchFaces(JSON.stringify(request), matchFacesResponse => {
+      const response = MatchFacesResponse.fromJson(JSON.parse(matchFacesResponse))
+  
       FaceSDK.matchFacesSimilarityThresholdSplit(JSON.stringify(response.results), 0.75, str => {
-        var split = MatchFacesSimilarityThresholdSplit.fromJson(JSON.parse(str))
-        this.setState({ similarity: split.matchedFaces.length > 0 ? ((split.matchedFaces[0].similarity * 100).toFixed(2) + "%") : "error" })
-      }, e => { this.setState({ similarity: e }) })
-    }, e => { this.setState({ similarity: e }) })
+        const split = MatchFacesSimilarityThresholdSplit.fromJson(JSON.parse(str))
+        setSimilarity({ similarity: split.matchedFaces.length > 0 ? ((split.matchedFaces[0].similarity * 100).toFixed(2) + "%") : "error" })
+
+      }, e => { setSimilarity({ similarity: e }) })
+    }, e => { setSimilarity({ similarity: e }) })
   }
 
-  liveness() {
-    FaceSDK.startLiveness(result => {
-      result = LivenessResponse.fromJson(JSON.parse(result))
-
-      this.setImage(true, result.bitmap, Enum.ImageType.LIVE)
-      if (result.bitmap != null)
-        this.setState({ liveness: result["liveness"] == 0 ? "passed" : "unknown" })
-    }, e => { })
+  // Clear Result
+  const clearResults = () => {
+    setImg1(
+      {uri: require('./images/portrait.png')}
+    ) 
+    setImg2(
+      {uri: require('./images/portrait.png')}
+    ) 
+    setImge1(new MatchFacesImage())
+    setImge2(new MatchFacesImage())
   }
+ 
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}> 
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <View style={{ flexDirection: "column", padding: 5 }}>
-            <View style={{ flexDirection: "column", alignItems: "center" }}>
-              <TouchableHighlight onPress={() => this.pickImage(true)}>
-                <Image
-                  style={{
-                    height: 150,
-                    width: 150,
-                  }}
-                  source={this.state.img1}
-                  resizeMode="contain" />
-              </TouchableHighlight>
-            </View>
-            <View style={{ flexDirection: "column", alignItems: "center", padding: 5 }}>
-              <TouchableHighlight onPress={() => this.pickImage(false)}>
-                <Image
-                  style={{
-                    height: 150,
-                    width: 200,
-                  }}
-                  source={this.state.img2}
-                  resizeMode="contain" />
-              </TouchableHighlight>
-            </View>
+        <View style={{ flexDirection: "column", padding: 5 }}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableHighlight onPress={() => pickImage(true)}>
+              <Image
+                style={{
+                  height: 150,
+                  width: 150,
+                }}
+                source={img1.uri}
+                resizeMode="contain" />
+            </TouchableHighlight>
           </View>
 
-          <View style={{ flexDirection: 'column', width: "100%", alignItems: "center" }}>
-            <View style={{ padding: 3, width: "75%" }}>
-              <Button color="#4285F4"
-                onPress={() => {
-                  this.matchFaces()
+          <View style={{ flexDirection: "column", alignItems: "center", padding: 5 }}>
+            <TouchableHighlight onPress={() => pickImage(false)}>
+              <Image
+                style={{
+                  height: 150,
+                  width: 200,
                 }}
-                title="     Match     "
-              />
-            </View>
-            <View style={{ padding: 3, width: "75%" }}>
-              <Button color="#4285F4"
-                onPress={() => {
-                  this.liveness()
-                }}
-                title="     Liveness     "
-              />
-            </View>
-            <View style={{ padding: 3, width: "75%" }}>
-              <Button color="#4285F4"
-                onPress={() => {
-                  this.clearResults()
-                }}
-                title="Clear"
-              />
-            </View>
-          </View>
-          <View style={{ flexDirection: 'row' }}>
-            <Text style={{ marginLeft: -20 }}>Similarity: {this.state.similarity}</Text>
-            <Text style={{ marginLeft: 20 }}>Liveness: {this.state.liveness}</Text>
+                source={img2.uri}
+                resizeMode="contain" />
+            </TouchableHighlight>
           </View>
         </View>
+
+        <View style={{ flexDirection: 'column', width: "100%", alignItems: "center" }}>
+          <View style={{ padding: 3, width: "75%" }}>
+            <Button color="#4285F4"
+              onPress={() => {
+                matchFaces()
+              }}
+              title="     Match     "
+            />
+          </View>
+
+          <View style={{ padding: 3, width: "75%" }}>
+            <Button color="#4285F4"
+              onPress={() => {
+                clearResults()
+              }}
+              title="Clear"
+            />
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ marginLeft: -20 }}>Similarity: {similarity.similarity}</Text>
+        </View>
       </View>
-    )
-  }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -176,3 +183,6 @@ const styles = StyleSheet.create({
     right: 20
   }
 })
+
+export default App;
+  
